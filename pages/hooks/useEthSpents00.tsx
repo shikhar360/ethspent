e481a42d537e10267b534b42cb26b6167e0e45d0
shikhar360 <sssonishikhar360@gmail.com>
2023-03-30 16:46:24 +0530

    geting data successfully

---
+++ pages/hooks/useEthSpents00.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { ethers ,  } from 'ethers';
+import { Web3Provider  , EtherscanProvider } from '@ethersproject/providers';
+
+
+export default async function useEthSpents(walletAddress : string) {
+  // const provider  = new EtherscanProvider("homestead" , process.env.NEXT_PUBLIC_APIKEY as string );
+  // const now = Date.now();
+  // const tenDaysAgo = now - 10 * 24 * 60 * 60 * 1000; // 10 days ago in milliseconds
+  // const transactions = await provider.getHistory(walletAddress)
+  // // console.log( process.env.NEXT_PUBLIC_APIKEY);
+  // const lastTx = transactions.slice(-10);
+  // console.log(lastTx);
+
+  // const date = lastTx?.map((tx) => {
+  //   if (!tx?.timestamp) return; // skip if no timestamp (e.g. contract creation
+  //   const txTimestamp = tx?.timestamp * 1000; // convert to milliseconds
+  //   const txDate = new Date(txTimestamp).toISOString().slice(0, 10); // extract YYYY-MM-DD
+  //   return txDate;
+  // })
+  // console.log(date);
+
+  // const valueSpent = lastTx?.map(async (tx) => {
+  //   const gettingTx = await provider.getTransaction(tx.hash);
+  //   return gettingTx.value.toString()
+
+  // })
+
+  // const valueSpent2 = await Promise.all(valueSpent);
+  // console.log(valueSpent2);
+
+ 
+  // const transactionDataByDay : any = {};
+
+  // transactions?.forEach((tx ) => {
+  //   if (!tx?.timestamp) return; // skip if no timestamp (e.g. contract creation
+  //   const txTimestamp = tx?.timestamp * 1000; // convert to milliseconds
+  //   const txDate = new Date(txTimestamp).toISOString().slice(0, 10); // extract YYYY-MM-DD
+
+  //   console.log(tx);
+  //   // const txValue = parseFloat(ethers.utils.formatEther(tx.value)); // convert from wei to ether
+  //   // if (transactionDataByDay[txDate]) {
+  //   //   transactionDataByDay[txDate] += txValue;
+  //   // } else {
+  //   //   transactionDataByDay[txDate] = txValue;
+  //   // }
+  // });
+
+
+}
---
+++ pages/hooks/useEtherTxData.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import {
+  EtherscanProvider,
+  Web3Provider,
+  AlchemyProvider,
+} from "@ethersproject/providers";
+
+import { ethers } from "ethers";
+
+export default async function useEtherTxData(
+  walletAddress: string,
+  numberOfDays: number = 30
+) {
+  try {
+    // const provider = new EtherscanProvider(
+    //   "homestead",
+    //   process.env.NEXT_PUBLIC_APIKEY as string
+    // );
+
+    //!........................................................
+
+    const daysAgo = numberOfDays;
+    const timestamp = Math.floor(Date.now() / 1000 - daysAgo * 24 * 60 * 60);
+
+    // Getting the closest block of the last provided days
+    const block = await fetch(
+      `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.NEXT_PUBLIC_APIKEY}`
+    );
+
+    const blockJson = await block.json();
+    // console.log(blockJson);
+
+    // Passing the block.result in the ap to ge the data
+
+    const response = await fetch(
+      `https://api.etherscan.io/api?module=account&action=txlist&address=${walletAddress}&startblock=${blockJson.result}&endblock=99999999&sort=asc&apikey=${process.env.NEXT_PUBLIC_APIKEY}`
+    );
+    // &start=${startTimestamp}&end=${endTimestamp}
+    const json = await response.json();
+    // console.log(json);
+
+    // now we have to create a map
+    const txByDate = groupping(json, walletAddress);
+    const arObj = Array.from(txByDate, ([date, txs]) => ({ date, txs }));
+    // console.log(arObj);
+    return arObj;
+    //!........................................................
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function groupping(json: any, walletAddress: string) {
+  const mappedData = new Map();
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  json.result.forEach((val: any) => {
+    // return const utcDate = new Date(val.timeStamp * 1000).toUTCString();
+    // console.log(val);
+    const utcDate = new Date(val.timeStamp * 1000).toUTCString();
+    const parsedDate = new Date(Date.parse(utcDate)).getDate();
+    const parsedMonth = new Date(Date.parse(utcDate)).getMonth();
+    const actualDate = months[parsedMonth] + " " + parsedDate;
+    const txValue =
+      val.from.toLowerCase() === walletAddress.toLowerCase()
+        ? +ethers.formatEther(val.value) * -1
+        : +ethers.formatEther(val.value);
+
+    if (mappedData.has(actualDate)) {
+      mappedData.get(actualDate).push(txValue);
+    } else {
+      mappedData.set(actualDate, [txValue]);
+    }
+  });
+
+  return mappedData;
+}
